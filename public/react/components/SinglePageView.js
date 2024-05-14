b23bbd05f58a3cae38d22380fda28e8872d99841
Mei Huang <meihuang8328@gmail.com>
2024-05-14 15:41:56 -0700

    added delete article functionality

--- public/react/components/SinglePageView.js
+++ public/react/components/SinglePageView.js
@@ -1,30 +1,57 @@
 import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import apiURL from '../api';
 import { Page } from './Page';
 
 const SinglePageView = () => {
   const { slug } = useParams();
   const [page, setPage] = useState(null);
+  // const history = useHistory();
+
+  const fetchPage = async () => {
+    try {
+      const response = await fetch(`${apiURL}/wiki/${slug}`);
+      const pageData = await response.json();
+      setPage(pageData);
+    } catch (err) {
+      console.log('Oh no an error!', err);
+    }
+  }
 
   useEffect(() => {
-    async function fetchPage() {
-      try {
-        const response = await fetch(`${apiURL}/wiki/${slug}`);
-        const pageData = await response.json();
-        setPage(pageData);
-      } catch (err) {
-        console.log('Oh no an error!', err);
-      }
-    }
     fetchPage();
   }, [slug]);
 
-  if (!page) {
-    return <div>Loading...</div>;
-  }
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`${apiURL}/wiki/${slug}`, {
+        method: 'DELETE'
+      });
+      if (response.ok) {
+        // Deletion successful, navigate back to the list of articles
+        // history.push('/');
+        window.location.href = '/';
 
-  return <Page page={page} />;
-}
+      } else {
+        console.error('Failed to delete page:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error deleting page:', error);
+    }
+  };
+
+  return (
+    <div>
+      {page ? (
+        <div>
+          <Page page={page} />
+          <button onClick={handleDelete}>Delete</button>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
 
 export default SinglePageView;
